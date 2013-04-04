baf783e6997fc9b36501731635dfa5e2910c7ff0
Sri Raghavan <sri.raghavan.1@gmail.com>
2013-04-04 13:56:51 -0400

    fix paths

--- js/homehunt.js
+++ js/homehunt.js
@@ -6,9 +6,9 @@ $(document).ready(function () {
   $('#actionbar h1').on('click', function () {
     var path = window.location.pathname;
     if (path.indexOf("loggedin") != -1 || path.indexOf("account") != -1) {
-      window.location.pathname = "/loggedin_home.html";
+      window.location.pathname = "loggedin_home.html";
     } else {
-      window.location.pathname = "/loggedout_home.html";
+      window.location.pathname = "loggedout_home.html";
     }
   });
 
@@ -23,9 +23,9 @@ $(document).ready(function () {
   $('.result').on('click', function () {
     var path = window.location.pathname;
     if (path.indexOf("loggedin") != -1 || path.indexOf("account") != -1) {
-      window.location.pathname = "/loggedin_house_detail.html";
+      window.location.pathname = "loggedin_house_detail.html";
     } else {
-      window.location.pathname = "/loggedout_house_detail.html";
+      window.location.pathname = "loggedout_house_detail.html";
     }
   })
 
